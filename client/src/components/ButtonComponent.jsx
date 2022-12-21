ef03130e5b393902219eed048619d56f36b3354e
= <=>
2022-12-21 09:46:49 -0500

    feat: add prop for custom button color

--- client/src/components/ButtonComponent.jsx
+++ client/src/components/ButtonComponent.jsx
@@ -3,10 +3,10 @@ import Button from 'react-bootstrap/Button';
 import { AiFillDelete } from 'react-icons/ai';
 import { MdOutlineDownloadDone } from 'react-icons/md';
 // eslint-disable-next-line react/prop-types
-export const ButtonComponent = ({ onClick, type = 'success', text }) => {
+export const ButtonComponent = ({ onClick, type = 'success', text, customColor }) => {
 	return (
 		<>
-			<Button variant={type} onClick={onClick}>
+			<Button variant={type} onClick={onClick} style={ customColor ? {backgroundColor: customColor, border:`1px solid ${customColor}`} : {} } >
 				<div className='flex flex-row items-center'>
 					{type === 'danger' ? <AiFillDelete className='mx-1' /> : null}
           {type === 'success' ? <MdOutlineDownloadDone className='mx-1' /> : null}
@@ -21,4 +21,5 @@ Button.propTypes = {
 	onClick: PropTypes.func,
 	type: PropTypes.string,
 	text: PropTypes.string,
+  customColor: PropTypes.string,
 };
